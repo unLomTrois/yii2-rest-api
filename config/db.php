76d68c9b52e84426e0fed284a6c5b5929d57a1f5
unlomtrois <vadim.kuz02@gmail.com>
2020-12-16 07:57:35 +0300

    connect postgres

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=yii2basic',
+    'username' => 'postgres',
     'password' => '',
     'charset' => 'utf8',
 
