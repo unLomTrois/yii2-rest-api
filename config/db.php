f61e1939e05fa248b614e60b8e0f95c36026a2e9
unlomtrois <vadim.kuz02@gmail.com>
2020-12-16 20:50:30 +0300

    add env variable for db stuff

--- config/db.php
+++ config/db.php
@@ -1,14 +1,16 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=test_task',
-    'username' => 'postgres',
-    'password' => '',
+    'dsn' => 'pgsql:host='.$url['host'].';port='.$url['port'].';dbname='.substr($url["path"], 1),
+    'username' => $url['user'],
+    'password' => $url['pass'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
 ];
